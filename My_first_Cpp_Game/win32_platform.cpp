e341c9a8b11ab8e081cfb8e223e3aea969e08dff
Kushch Oleg <o.kushch@netbox.ru>
2021-03-08 15:55:04 +0300

    initial commit

---
+++ My_first_Cpp_Game/win32_platform.cpp
@@ -0,0 +1,104 @@
+#include <windows.h>
+
+bool is_running = true;
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+Render_State render_state;
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(
+	_In_ HWND   hwnd,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+) {
+	LRESULT result = 0;
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			is_running = false;
+		} break;
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;			
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}		 
+	}
+
+	return result;
+};
+
+int WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nShowCmd
+) {
+
+	// Create window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = L"Game window Class";
+	window_class.lpfnWndProc = window_callback;
+	//Register that Class
+	RegisterClass(&window_class);
+	//Create window
+
+	HWND window = CreateWindow(
+		window_class.lpszClassName,
+		L"My first Game",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		800,
+		600,
+		0,
+		0,
+		hInstance,
+		0
+	);
+
+	HDC hdc = GetDC(window);
+
+	while (is_running) {
+		// Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		// Simulate
+		render_background();
+		clear_screen(0xff5500);
+		draw_rect(50, 50, 200, 500, 0xff0000);
+
+		// Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	}
+
+};
